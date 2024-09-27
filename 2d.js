e48a9732b7e4d88cf54b535347dacc3186a01b93
TiffanyYongNgikChee <G00425067@atu.ie>
2024-09-27 09:03:35 +0000

    2d finished

---
+++ 2d.js
@@ -0,0 +1,11 @@
+//a function that multiply each number under 70 by 2 in the following array.
+const ages = [25, 31, 42, 77];
+
+let d = ages.map((item)=>{
+    if(item<70)
+        return item*2;
+    else 
+        return item;
+    })
+
+console.log(d);
